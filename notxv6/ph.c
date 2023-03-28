4eaddbcf22d77f4b49255db3cfb60eb5f2733f56
Tarushii Goel <tarushii@mit.edu>
2023-03-27 22:28:09 -0400

    add lock

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lock;            // declare a lock
 
 double
 now()
@@ -52,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock);       // acquire lock
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock);     // release lock
   }
 
 }
@@ -117,6 +119,7 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+  pthread_mutex_init(&lock, NULL); // initialize the lock
 
   //
   // first the puts
